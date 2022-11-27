43b7c89be659a3e8485d7748b5b6bfa2a884291c
brianmbutler <brian_m_butler@icloud.com>
2022-11-27 14:52:29 -0500

    upgraded quiz to tkinter

--- day34/data.py
+++ day34/data.py
@@ -1,28 +1,8 @@
 import requests as r
-import pandas
 
 params = {
     'amount': 10,
     'type': 'boolean'
 }
 
-response = r.get(url='https://opentdb.com/api.php', params=params)
-data = response.json()
-
-df = pandas.read_json(data)
-
-
-question_data = [  for (index, data_row) in df.
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The HTML5 standard was published in 2014.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-   
-    
-]
+question_data = r.get(url='https://opentdb.com/api.php', params=params).json()['results']
